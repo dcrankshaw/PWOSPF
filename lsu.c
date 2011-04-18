88fd2fc070edf421cb4f482492a6bd725e8849bb
Madeline Stone <mstone9@jhu.edu>
2011-04-18 16:07:50 -0400

    lsu_handle written, but not compiled.

--- lsu.c
+++ lsu.c
@@ -8,40 +8,37 @@ void handle_lsu(struct ospfv2_hdr* pwospf, struct packet_state* ps)
         //DROP
     }
     
-    ps->packet=ps->packet + sizeof(struct ospfv2_hdr); /* Now packet points to beg of lsu hdr */
+    
+    ps->packet=ps->packet + sizeof(struct ospfv2_hdr); 
     struct ospfv2_lsu_hdr* lsu_head = (struct ospfv2_lsu_hdr*)(ps->packet);
     
-    struct lsu_rcd* rcd_lsu_walker=ps->sr->lsu_info->rcd_lsus;
-    while(rcd_lsu_walker)
+    uint16_t old_seq= get_sequence(pwospf_rid, ps->sr);
+    if(old_seq==lsu_head->seq)
     {
-        if(rcd_lsu_walker->source_rid==pwospf->rid)
-            if(rcd_lsus->seq==lsu_head->seq)
-            {
-                //DROP
-            }
-        rcd_lsu_walker=rcd_lsu_walker->next;
+        //DROP
+    }
+    uint32t_t source_rid=pwospf->rid;
+    struct route* advertisements=(struct route*)malloc(lsu_head->num_adv); //Can't rem if this is how to do this
+    
+   
+    for(int i=0; i<lsu_head->num_adv; i++)
+    {
+         ps->packet=ps->packet+sizeof(struct ospfv2_lsu-hdr);
+        struct ospfv2_lsu_adv* temp_adv=(struct ospfv2_lsu_adv*)(ps->packet);
+        
+        struct route temp_rt;
+        temp_rt.prefix=temp_adv->subnet;
+        temp_rt.mask=temp_adv->mask;
+        temp_rt.r_id=temp_adv->rid;
+        
+        advertisement[i]=temp_rt;
     }
     
-    /* Check if contents match contents of previous entry*/
-    /******** How do I want to do this?? -MS 
-    * Possibly don't do anything special just check database if any new information 
-    * needs to be added. If it does because 1) it's new or 2) it's from a new host, 
-    * update DB and recomputer FT. If no new info, just update DB.
-    ps->packet=ps->packet + sizeof(struct ospfv2_lsu_hdr);  
-    rcd_lsu_walker=ps->sr->lsu_info->rcd_lsus;
-    while(rcd_lsu_walker)
-    {
-        if(rcd_lsu_walker->source_rid==pwospf->rid)
-        {
-            struct ospfv2_lsu_adv* adv_walker=rcd_lsu_walker->adv_list;
-            while(adv_walker)
-            {
-                
-            }
-        }    
-        rcd_lsu_walker=rcd_lsu_walker->next;
-    }
-    */   
+    /* Adds advertisements to topology if necessary and recomputes FT if necessary.*/
+    add_to_top(ps->sr, source_rid, &advertisements[0], lsu_head->num_adv);
+  
+    set_sequence(pwospf->rid, lsu_head->seq, ps->sr);
+  
 }
 
 void send_lsu()
