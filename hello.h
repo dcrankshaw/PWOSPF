8c08ad0ef0ba4fdeefa18fbc02b13879e6e7dc83
Adam Gross <adamgross@jhu.edu>
2011-04-19 00:02:55 -0400

    Changed variable names to match sr_pwospf.h

--- hello.h
+++ hello.h
@@ -16,6 +16,8 @@
 #include "sr_protocol.h"
 #include "pwospf_protocol.h"
 
+/*
+
 #define ALLSPFRouters 0xe0000005
 
 struct interface_list_entry
@@ -35,6 +37,8 @@ struct neighbor_list_entry
     time_t timenotvalid;                /*The time when this entry is no longer valid*/
     struct neighbor_list_entry* next;
 };
+
+*/
 
 void handle_HELLO(struct packet_state*, struct sr_ethernet_hdr*);
 struct neighbor_list_entry* delete_neighbor_list_entry(struct interface_list_entry*, struct neighbor_list_entry*);
--- sr_pwospf.h
+++ sr_pwospf.h
@@ -71,10 +71,11 @@ struct ftable_entry
 
 struct pwospf_iflist
 {
+	char name[sr_IFACE_NAMELEN];
 	struct in_addr address;
 	struct in_addr mask;
-	char name[sr_IFACE_NAMELEN];
 	uint16_t helloint;
+	unsigned char mac[ETHER_ADDR_LEN];
 	struct neighbor_list *neighbors;
 	struct pwospf_iflist *next;
 };
@@ -83,6 +84,7 @@ struct neighbor_list
 {
 	uint32_t id;
 	struct in_addr ip_address;
+	time_t timenotvalid;                /*The time when this entry is no longer valid*/
 	struct neighbor_list *next;
 };
 
